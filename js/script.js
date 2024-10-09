2fc927c64acec64780cf605338d6166a61186974
michalk1924 <michalk1924@gmail.com>
2024-10-09 13:19:25 +0300

    add fectch clothes

--- js/script.js
+++ js/script.js
@@ -16,16 +16,52 @@ const getProductDiv = (product) => {
     </div>
     `
 }
-  
-const renderProducts = () => {
-    const productsDiv = currentPage === 'notebooks' ? notebooks.map(getProductDiv).join(''):
-    craft.map(getProductDiv).join('');
-    prodContainer.innerHTML = productsDiv;
-    pageTitle.innerHTML = currentPage === 'notebooks'? 'מחברות' : 'מכשירי כתיבה';
 
+const renderProducts = async () => {
+    let productsDiv;
+    switch (currentPage) {
+        case 'notebooks':
+            {
+                productsDiv = notebooks.map(getProductDiv).join('');
+                pageTitle.innerHTML = 'מחברות';
+                break;
+            }
+        case 'craft':
+            {
+                productsDiv = craft.map(getProductDiv).join('');
+                pageTitle.innerHTML = 'מכשירי כתיבה';
+                break;
+            }
+        case 'clothes':
+            {
+                try {
+                    productsDiv = await fetchClothes();
+
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+                break;
+            }
+        default:
+            break;
+    }
+    prodContainer.innerHTML = productsDiv;
 }
 
 const changePage = (pageCategory) => {
     currentPage = pageCategory;
     renderProducts();
+}
+
+const fetchClothes = async () => {
+    const response = await fetch('/https://fakestoreapi.com');
+    if (response.ok) {
+        const data = await response.json();
+        const productsDiv = data.map(getProductDiv).join('');
+        pageTitle.innerHTML = 'בגדים';
+    }
+    else {
+        throw new Error('Error fetching data from API');
+    }
+    return productsDiv;
 }
